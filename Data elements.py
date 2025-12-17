744763bc3ed771c9ecc2dcd8332d47aaf9d4ffd5
ivanliu0527-cpu <ivanliu0527@gmail.com>
2025-12-17 14:19:57 +0800

    Create Data elements.py

---
+++ Data elements.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import streamlit as st
+
+data = {
+    'Product': ['A', 'B', 'C'],
+    'Sales': [1200, 850, 950],
+    'Customers': [300, 400, 350]
+}
+df = pd.DataFrame(data)
+
+st.dataframe(df)
+st.data_editor(df)
+st.table(df)
+
+st.dataframe(df.style.format({'Sales': '${:,.0f}', 'Customers': '{:,.0f}'}))
